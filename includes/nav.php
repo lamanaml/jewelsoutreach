e75970f4f97f26577917c3695eb4c1e461b180bf
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-03 13:23:14 -0500

    initial commit

--- includes/nav.php
+++ includes/nav.php
@@ -1,13 +1,3 @@
-<nav>
-					<h1 class="logo">Jewels Networking and Outreach</h1>
-					<ul>
-						<li><a href="#">Home</a></li>
-						<li><a href="#">Ministries</a></li>
-                  
-                  <li><a href="#">Events</a></li>
-						<li><a href="#">About</a></li>
-					</ul>
-				</nav>
 
 <nav class="navi">
     <ul class="navi-list">
