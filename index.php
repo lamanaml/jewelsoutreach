e4da0ed1328b0195aff36f65febeee10f0a11e2e
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-03 13:30:27 -0500

    initial commit

--- index.php
+++ index.php
@@ -16,33 +16,21 @@
       include ('includes/nav.php');
    ?>
 
-   <!-- main content -->
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-         <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-         </ol>
-         <div class="carousel-inner">
-            <div class="carousel-item active">
-               <img src="assets/images/servingfood.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-               <img src="assets/images/servingfood.jpg." class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-               <img src="assets/images/servingfood.jpg" class="d-block w-100" alt="...">
-            </div>
-         </div>
-         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-         </a>
-         <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-         </a>
-         </div>
+
+      
+
+    
+
+      
+<section class="masthead" role="img" aria-label="Image Description">
+  <h1>
+    The Hero Generator
+  </h1>
+    <button>
+      When a hero comes along
+    </button>
+</section>
+    
 
 	<?php
       include ('includes/footer.php');
