c19b1d198bccdfd63568fd8e05d0259994c0aa97
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-03 11:48:50 -0500

    initial commit

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/dist/style.css">
    <title>Jewels Networking and Outreach</title></title>
 </head>
@@ -11,6 +12,9 @@
   
 <header class="showcase">
 			<div class="container">
+	<?php
+      include ('includes/nav.php');
+   ?>
 
 				<div class="showcase-content">
 					<div>
