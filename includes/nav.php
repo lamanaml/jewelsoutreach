8e00860af25b113c3219dd8c75f3cb7eddc83470
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-03 09:53:54 -0500

    initial commit

---
+++ includes/nav.php
@@ -0,0 +1,10 @@
+<nav>
+					<h1 class="logo">Jewels Networking and Outreach</h1>
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Ministries</a></li>
+                  
+                  <li><a href="#">Events</a></li>
+						<li><a href="#">About</a></li>
+					</ul>
+				</nav>
---
+++ index.php
@@ -0,0 +1,25 @@
+
+<link rel="stylesheet" href="assets/css/dist/style.css">
+<header class="showcase">
+			<div class="container">
+	<?php
+     include ('includes/nav.php');
+?>
+
+				<div class="showcase-content">
+					<div>
+						<h1>Make Your Marketing Real</h1>
+						<p class="my-1">
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est
+							eligendi tempore atque laborum. Quisquam nemo at non. Corrupti,
+							vitae dolore.
+						</p>
+						<a href="#" class="btn-primary">Learn More</a>
+						<a href="#" class="btn-secondary">Sign Up</a>
+					</div>
+					<img
+						src="assets/images/servingfood.jpg"
+					/>
+				</div>
+			</div>
+		</header>
