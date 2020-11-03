37f7f8e3c60900f998358e22aecc5cbb9c80c4ec
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-03 11:58:04 -0500

    initial commit

--- index.php
+++ index.php
@@ -26,13 +26,13 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="assets/images/servingfood.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="assets/images/servingfood.jpg." class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="assets/images/servingfood.jpg" class="d-block w-100" alt="...">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
