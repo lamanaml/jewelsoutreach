978c58c4835828bb29cf17fb4131a7d48e4a7d63
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-03 11:43:25 -0500

    initial commit

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 
-<link rel="stylesheet" href="assets/css/dist/style.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/dist/style.css">
+   <title>Jewels Networking and Outreach</title></title>
+</head>
+<body>
+  
 <header class="showcase">
 			<div class="container">
 	<?php
@@ -23,3 +32,5 @@
 				</div>
 			</div>
 		</header>
+</body>
+</html>
