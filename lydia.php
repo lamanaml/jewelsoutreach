277beff4fa9ac9ead02f64f0b0df8cd145a448d8
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-20 16:28:21 -0500

    updated slideshow

--- lydia.php
+++ lydia.php
@@ -192,9 +192,9 @@
         <!-- Core theme JS-->
                
         <script src="js/scripts.js"></script>
-     
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
         <script src="js/slides.js"></script>
-             <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+          
   
 
     </body>
