c01046a1aebeeaf74c86f0f92bbd3ba2afa1b2a9
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-20 15:16:17 -0500

    updated card divs

--- lydia.php
+++ lydia.php
@@ -163,30 +163,6 @@
 
 
 
-  <div class="container" >
-    <h2 class="my-3">Daughters of Lydia</h2> -->
-    <p class="text-muted">Women Entrepreneurs coming together for Networking and Collective Philanthropy. We encourage and equip Women Entrepreneurs to build and impact their families, business  and engage in their community by using their gifts, talents and resources all while sharing the love of God. God has called us to be Kingdom Financiers!<br/><br/>
-    <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/DaughtersLYDIA">
-    <i class="fab fa-facebook-f"></i></a> </p>
-  </div> -->
-
-
-
- <!-- Footer-->
-
-
-
-        <!-- Bootstrap core JS-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
-    
-        <!-- Third party plugin JS-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-        <!-- Contact form JS-->
-        <script src="assets/mail/jqBootstrapValidation.js"></script>
-        <script src="assets/mail/contact_me.js"></script>
-        <!-- Core theme JS-->
-                <script src="js/slides.js"></script>
         <script src="js/scripts.js"></script>
 
 
