d031b82c61d06afe44889439ea66bedcf4d96709
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-20 17:05:48 -0500

    updated slideshow

--- index.php
+++ index.php
@@ -79,10 +79,10 @@
                 <div class="row">
                     <div class="col-lg-6 mb-4">
                         <div class="card h-100">
-                            <a href="lydia.html"><img class="card-img-top" src="assets/img/events/gallery/21.jpg" alt=""></a>
+                            <a href="lydia.php"><img class="card-img-top" src="assets/img/events/gallery/21.jpg" alt=""></a>
                             <div class="card-body">
                                 <h4 class="card-title">
-                                    <a href="#">Daughters of Lydia</a>
+                                    <a href="lydia.php">Daughters of Lydia</a>
                                 </h4>
                                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
                                 <a class="btn btn-secondary btn-xl text-uppercase js-scroll-trigger" href="lydia.php">Learn More</a> 
@@ -91,13 +91,13 @@
                     </div>
                     <div class="col-lg-6 mb-4">
                         <div class="card h-100">
-                            <a href="feedingfriends.php"><img class="card-img-top" src="assets/img/events/gallery/1.jpg" alt=""></a>
+                            <a href="feeding.php"><img class="card-img-top" src="assets/img/events/gallery/1.jpg" alt=""></a>
                             <div class="card-body">
                             <h4 class="card-title">
-                                <a href="jewels.php">Jewels Feeding friends</a>
+                                <a href="feeding.php">Jewels Feeding friends</a>
                             </h4>
                             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit aliquam aperiam nulla perferendis dolor nobis numquam, rem expedita, aliquid optio, alias illum eaque. Non magni, voluptates quae, necessitatibus unde temporibus.</p>
-                            <a class="btn btn-secondary btn-xl text-uppercase js-scroll-trigger" href="feedingfriends.php">Learn More</a> 
+                            <a class="btn btn-secondary btn-xl text-uppercase js-scroll-trigger" href="feeding.php">Learn More</a> 
                             </div>
                         </div>
                     </div>
