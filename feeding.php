3caad17f322aa91e0c19ebf49be6fefacc8e3aff
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-21 02:08:49 -0500

    updated feeding images

--- feeding.php
+++ feeding.php
@@ -151,9 +151,8 @@
         <!-- Services-->
 <div class="container">
 
- 
-         <div class="container" > <h4 class="my-3">JEWELS Feeding Friends</h4>
-               <p class="text-muted">We provide solutions for the food insecurities in our community. Jewels Feeding Friends has taken on the mission to help meet needs in our community for men, women, children of all ages and walks of life. Through partnerships, we are able to help combat this growing epidemic of hunger. <br/>     <br/> 
+  <h2 class="my-3">Daughters of Lydia</h2> 
+              <h3 class="text-muted">We provide solutions for the food insecurities in our community. Jewels Feeding Friends has taken on the mission to help meet needs in our community for men, women, children of all ages and walks of life. Through partnerships, we are able to help combat this growing epidemic of hunger. </h3><br/>     <br/> 
                
               <a class="btn btn-secondary btn-xl text-uppercase js-scroll-trigger" href="#contact">Volunteer</a> <a class="btn btn-dark btn-social mx-2" href="feedingfriends.html"><i class="fab fa-facebook-f"></i></a></p></div>
          </div>
