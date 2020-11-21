076ad31f36c6e43d389296509b66fb89a21e040e
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-21 01:52:56 -0500

    updated cardsnavigation

--- feeding.php
+++ feeding.php
@@ -49,7 +49,7 @@
             include 'includes/nav.php';
         ?>
         <!-- Masthead-->
-        <header class="masthead2">
+        <header class="masthead3">
             <div class="container">
                 <!-- <div class="masthead-subheading">Jewels Networking and Outreach</div> -->
                 <div class="masthead-heading text-uppercase">Jewels Feeding Friends  </div>               
