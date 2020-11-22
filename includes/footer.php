2618430d6425896c598bc1a8941cd89e2d77776e
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-22 13:11:24 -0500

    updated feeding content

--- includes/footer.php
+++ includes/footer.php
@@ -27,7 +27,7 @@
 		<div class="response" id="mce-success-response" style="display:none"></div>
 	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f8fb542db59835608c24db966_e0b5c68a86" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-secondary btn-xl text-uppercase js-scroll-trigger "></div>
     </div>
 </form>
 </div>
