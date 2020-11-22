64e5dde398e42702142653f75d5fc33cb83800e7
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-22 13:23:45 -0500

    updated feeding content

--- includes/footer.php
+++ includes/footer.php
@@ -6,14 +6,12 @@
 
 	<label for="mce-EMAIL">Email Address </label>
 	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-
-
-<div id="mce-responses" class="clear">
+    <span id="mce-responses" class="clear">
 		<div class="response" id="mce-error-response" style="display:none"></div>
 		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+	</span>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <span style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f8fb542db59835608c24db966_e0b5c68a86" tabindex="-1" value=""></span>
-   <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-secondary btn-xl text-uppercase js-scroll-trigger ">
+    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-secondary btn-xl text-uppercase js-scroll-trigger ">
 
 </form>
 </div>
