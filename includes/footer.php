deb63c34bd2d016ec499ea771fc9561c7726c7cd
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-22 23:35:50 -0500

    updated contact

--- includes/footer.php
+++ includes/footer.php
@@ -11,7 +11,7 @@
 <div id="mc_embed_signup">
 <form action="https://jewelsoutreach.us7.list-manage.com/subscribe/post?u=f8fb542db59835608c24db966&amp;id=e0b5c68a86" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
-	<label for="mce-EMAIL">Subscribe</label>
+	 <h2 class="section-heading text-uppercase">Subscribe for updates!</h2>
 	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f8fb542db59835608c24db966_e0b5c68a86" tabindex="-1" value=""></div>
@@ -23,22 +23,6 @@
 <!--End mc_embed_signup-->
 
 
-    <!-- <div id="mc_embed_signup">
-        <form  class="form-inline" action="https://jewelsoutreach.us7.list-manage.com/subscribe/post?u=f8fb542db59835608c24db966&amp;id=e0b5c68a86" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-            <div id="mc_embed_signup_scroll">
-                <h2 class="section-heading text-uppercase">Subscribe for updates!</h2>
-                <input type="email" value="" name="EMAIL" placeholder="Email Address *" class="required email form-control" id="mce-EMAIL" aria-describedby="emailHelp" >
-                <span id="mce-responses" class="clear">
-                    <div class="response" id="mce-error-response" style="display:none"></div>
-                    <div class="response" id="mce-success-response" style="display:none"></div>
-                </span>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                <!-- <span style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f8fb542db59835608c24db966_e0b5c68a86" tabindex="-1" value=""></span> -->
-                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-secondary btn-xl text-uppercase js-scroll-trigger ">
-        </form>
-            </div>
-    </div>
-</div> -->
-<!--End mc_embed_signup-->
 
 
 <!-- Footer-->
