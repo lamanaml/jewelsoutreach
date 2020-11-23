c21efda8e00fea16c3c6d24e28db478fa6482d68
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-23 00:23:12 -0500

    updated contact

--- lydia.php
+++ lydia.php
@@ -54,7 +54,7 @@
             </div>
         </header>
         <!-- Services -->
-        <div class="container2" style="align-items:center"> 
+        <div style="align-items:center"> 
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -62,19 +62,19 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
   </ol>
-  <div class="carousel-inner"style="width=100%">
+  <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="assets/img/events/gallery/dolbusiness.jpg"  style="width=100%" alt="First slide">
+      <img class="d-block w-100" src="assets/img/events/gallery/dolbusiness.jpg"  alt="First slide">
     </div>
-    <div class="carousel-item" style="width=100%">
-      <img class="d-block w-100" src="assets/img/events/gallery/dolpark.jpg"  style="width=100%"alt="Second slide">
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/img/events/gallery/dolpark.jpg" alt="Second slide">
     </div>
-    <div class="carousel-item" style="width=100%">
-      <img class="d-block w-100" src="assets/img/events/gallery/dollake.jpg" style="width=100%" alt="Third slide">
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/img/events/gallery/dollake.jpg" alt="Third slide">
     </div>
   </div>
-  <div class="carousel-item" style="width=100%">
-      <img class="d-block w-100" src="assets/img/events/gallery/dolhealthfair.jpg" style="width=100%" alt="Third slide">
+  <div class="carousel-item">
+      <img class="d-block w-100" src="assets/img/events/gallery/dolhealthfair.jpg" alt="Third slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
