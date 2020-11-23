b719c1f7a7eec2daef5f9bf72b65e2bc79e4f9ea
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-22 23:31:40 -0500

    updated contact

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <!-- Begin Mailchimp Signup Form -->
-<div class="container" style="background-color:pink;">
+<div  style="background-color:pink;">
     <div id="mc_embed_signup">
         <form  class="form-inline" action="https://jewelsoutreach.us7.list-manage.com/subscribe/post?u=f8fb542db59835608c24db966&amp;id=e0b5c68a86" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
             <div id="mc_embed_signup_scroll">
