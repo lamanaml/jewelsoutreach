6d3beddeade68e46710c9f2c5bdce3f4411342c8
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-22 23:09:32 -0500

    updated contact

--- includes/footer.php
+++ includes/footer.php
@@ -19,47 +19,12 @@
 </div>
 <!--End mc_embed_signup-->
 
-<!--End mc_embed_signup-->
 <!-- Contact-->
         <section class="page-section" id="contact">
             <div class="container">
 
 
-<!--End mc_embed_signup-->
-                <div class="text-center">
-                    <h2 class="section-heading text-uppercase">Prayer Request or Contact Us</h2>
-                    <h3 class="section-subheading text-muted">In need of prayer for yourself or someone you know?  Email us below or call us at (919) 438-0374</h3>
-                </div>
-                <form id="contactForm" name="sentMessage" novalidate="novalidate">
-                    <div class="row align-items-stretch mb-5">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <input class="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
-                                <p class="help-block text-danger"></p>
-                            </div>
-                            <div class="form-group">
-                                <input class="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address." />
-                                <p class="help-block text-danger"></p>
-                            </div>
-                            <div class="form-group mb-md-0">
-                                <input class="form-control" id="phone" type="tel" placeholder="Your Phone "  data-validation-required-message="Please enter your phone number." />
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group form-group-textarea mb-md-0">
-                                <textarea class="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="text-center">
-                        <div id="success"></div>
-                        <button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Send Message</button>
-                    </div>
-                </form>
-            </div>
-        </section> 
+
         <!-- Footer-->
         <footer class="footer py-4">
             <div class="container">
