67b5ff2e19d8140f14ee35058d48361a13e2c760
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-23 00:18:32 -0500

    updated contact

--- includes/footer.php
+++ includes/footer.php
@@ -27,9 +27,9 @@
     <footer class="footer py-4">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-lg-6 text-lg-center">Copyright © Jewels Networking and Outreach 2016</div>
-                               <div class="col-lg-4 text-lg-right">
-                    <!-- <a class="mr-3" href="#!">Privacy Policy</a>
+                <div class="col-lg-8">Copyright © Jewels Networking and Outreach 2016</div>
+                              <!--  <div class="col-lg-4 text-lg-right">
+                    <a class="mr-3" href="#!">Privacy Policy</a>
                     <a href="#!">Terms of Use</a> -->
                 </div>
             </div>
