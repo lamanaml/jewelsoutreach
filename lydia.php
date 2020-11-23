fc86dd193f8168ca64314cf287b851a84db7ae95
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-23 00:23:46 -0500

    updated contact

--- lydia.php
+++ lydia.php
@@ -35,7 +35,7 @@
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link href="css/bootstrap.css" rel="stylesheet" />
         <link href="css/jewels.css" rel="stylesheet" />
-        <link href="css/dist/slides.css" rel="stylesheet" />
+        <!-- <link href="css/dist/slides.css" rel="stylesheet" /> -->
 
     </head>
 
