05569fdf2802380b4c2065655683a01956fcd745
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-22 23:37:40 -0500

    updated contact

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,12 @@
-<!-- Begin Mailchimp Signup Form -->
-<div  style="background-color:pink;">
 
 <!-- Begin Mailchimp Signup Form -->
 <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
 <style type="text/css">
-	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+	#mc_embed_signup{background:#pink; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
 	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
 	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 </style>
-<div id="mc_embed_signup">
+<div id="mc_embed_signup"> 
 <form action="https://jewelsoutreach.us7.list-manage.com/subscribe/post?u=f8fb542db59835608c24db966&amp;id=e0b5c68a86" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
 	 <h2 class="section-heading text-uppercase">Subscribe for updates!</h2>
@@ -18,7 +16,7 @@
     <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
     </div>
 </form>
-</div>
+
 
 <!--End mc_embed_signup-->
 
