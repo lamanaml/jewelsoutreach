0f370626b98d7fd4a18cae9ca7ec54ca7562887f
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-23 00:02:41 -0500

    updated contact

--- lydia.php
+++ lydia.php
@@ -54,8 +54,39 @@
             </div>
         </header>
         <!-- Services -->
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="assets/img/events/gallery/dolbusiness.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/img/events/gallery/dolpark" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="assets/img/events/gallery/dollake.jpg" alt="Third slide">
+    </div>
+  </div>
+  <div class="carousel-item">
+      <img class="d-block w-100" src="assets/img/events/gallery/dolhealthfair.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
-
+<!-- 
 <main class="main-content">
   <section class="slideshow">
     <div class="slideshow-inner">
@@ -147,7 +178,7 @@
       </div>
     </div> 
   </section>
-</main>
+</main> -->
 
 
         <section class="page-section" >
