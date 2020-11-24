5fa9669bc000dea23100ae2d5671705b46b5a557
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-23 20:43:09 -0500

    updated images

--- feeding.php
+++ feeding.php
@@ -102,11 +102,14 @@
 
 
 
-                <br/> <br/>
-                <h3> 2020 Thanksgiving in the Park!</h3>
+                
+                <h3> 2017 Thanksgiving Lunch in the Park!</h3>
                 <div class="iframe">
-                  <iframe src="https://drive.google.com/file/d/1IPUvZAXaXHpNcr_QtoMEXXAY87DIQefF/preview" ></iframe>
-                </div>
+                  <iframe src="https://www.youtube.com/embed/H57ILMJh-tI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div><br/> 
+
+
+                
                 <!-- <br/> <br/>
                 <h3> 2017 Thanksgiving Lunch in the Park!</h3>
                 <div class="iframe">
@@ -122,10 +125,11 @@
           <div class="col-lg-6 mb-4">
             <div class="card h-100" style="border:0px">
               <div class="card-body" style="text-align:center;">
-               <h3> 2017 Thanksgiving Lunch in the Park!</h3>
+               <h3> 2020 Thanksgiving in the Park!</h3>
                 <div class="iframe">
-                  <iframe src="https://www.youtube.com/embed/H57ILMJh-tI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  <iframe src="https://drive.google.com/file/d/1IPUvZAXaXHpNcr_QtoMEXXAY87DIQefF/preview" ></iframe>
                 </div>
+
                 <br/>
                 <br/>
                 <h4>How can you help?</h4>
