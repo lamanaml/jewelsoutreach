200d520e5ee12b230602d68d72f73dc45499aeb9
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-23 19:26:43 -0500

    updated images

--- contact.php
+++ contact.php
@@ -70,16 +70,19 @@
 
                 <!-- https://us7.list-manage.com/contact-form?u=f8fb542db59835608c24db966&form_id=272a51c4fc8761bf753113a9188d5253 -->
                 <form id="contactForm" name="sentMessage" novalidate="novalidate">
-                     <div class="row align-items-stretch mb-5">
-                    <h3>Subject</h3>
-                        <select class="form-control" id="contact" name="contact">
-                        <option value="prayer">Prayer Request</option>
-                        <option value="donate">Donate Items</option>
-                        <option value="volunteer">Become a Volunteer</option>
-                        <option value="sponsor">Become a Sponsor</option>
-                        <option value="question">Questions or Comments</option>
-                        </select>
+                    <div class="row align-items-stretch mb-5">
+                        <div class="col-md-6">
+                            <h3>Subject</h3>
+                            <select class="form-control" id="contact" name="contact" required="required" data-validation-required-message="Please Select a Subject">
+                                <option value="prayer">Prayer Request</option>
+                                <option value="donate">Donate Items</option>
+                                <option value="volunteer">Become a Volunteer</option>
+                                <option value="sponsor">Become a Sponsor</option>
+                                <option value="question">Questions or Comments</option>
+                                <option value="other">Other</option>
+                            </select>
                         </div>
+                    </div>
                     <div class="row align-items-stretch mb-5">
                         <div class="col-md-6">
                             <div class="form-group">
