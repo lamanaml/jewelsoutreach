4052d17cd1eb7efc5068e40a33530eae4f67fb2d
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-25 17:15:45 -0500

    updated contact address

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -9,7 +9,7 @@ if(empty($_POST['name'])      ||
    echo "No arguments Provided!";
    return false;
    }
-$subject = strip_tags(htmlspecialchars($_POST['subject']));  
+// $subject = strip_tags(htmlspecialchars($_POST['subject']));  
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
