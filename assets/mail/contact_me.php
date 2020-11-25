291066132c4416b40f12e5dafc6ce9f44cb25dba
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2020-11-25 11:10:56 -0500

    updated contact form

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -17,8 +17,8 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'lamana@lamanadonadelle.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Jewels Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nsubject: $subject\n\n\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_subject = "Jewels Contact Form:  $subject";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: lamana@lamanadonadelle.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
